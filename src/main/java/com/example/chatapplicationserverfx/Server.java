0dbdd4e3f4a625971211e6ce85afb57b5439b7f1
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-07 20:03:21 +0000

    Finished the server class

--- src/main/java/com/example/chatapplicationserverfx/Server.java
+++ src/main/java/com/example/chatapplicationserverfx/Server.java
@@ -1,5 +1,7 @@
 package com.example.chatapplicationserverfx;
 
+import javafx.scene.layout.VBox;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -25,11 +27,49 @@ public class Server {
                 e.printStackTrace();
                 System.out.println("ERROR CREATING THE SERVER.");
 
+
             }
 
 
     }
+    public void sendMessageToClient(String messageToSend){
+        try {
+            bufferedWriter.write(messageToSend);
+            bufferedWriter.newLine(); // need to send overr a new line so it knows the message has ended
+            bufferedWriter.flush(); // manually flushing the buffere because it won't be full
 
+
+        } catch(IOException e){
+            e.printStackTrace();
+            System.out.println("Error sending message to client.");
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+
+    }
+    // Need to run this on a new thread so the program is not constantly waiting to receive messages, it can also send them
+    public void receiveMessageFromClient(VBox vBox){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(socket.isConnected()) {
+                    String messageFromClient = null;
+                    try {
+                        messageFromClient = bufferedReader.readLine();
+                        HelloController.addLabel(messageFromClient, vBox);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.out.println("Error receiving message from the client.");
+                        closeEverything(socket, bufferedReader, bufferedWriter);
+                        break;
+                    }
+
+                }
+
+
+            }
+        }).start();
+
+    }
 
     // Method to close all the resources that we are using - prevents null pointer exceptions
     public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
